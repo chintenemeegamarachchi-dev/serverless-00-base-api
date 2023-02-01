e059316d89f3d94a5ce406e8e8c77fadfac367d5
Chintana Meegamarachchi <chintana@appsolve.com.au>
2023-02-02 07:18:48 +0800

    Initial version

---
+++ handler.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const DynamoDb = require(`aws-sdk/clients/dynamodb`);
+const documentClient = new DynamoDb.DocumentClient({region: process.env.REGION});
+const NOTES_TBL_NAME = process.env.NOTES_TBL_NAME;
+
+const send = (statusCode, body) => {
+  return {
+    statusCode,
+    body
+  }
+}
+
+module.exports.createNote = async (event, context, callBack) => {
+  const {id, title, ...body} = JSON.parse(event.body);
+  try {
+    const param = {
+      TableName: NOTES_TBL_NAME,
+      Item: {
+        notesId: id,
+        title,
+        body
+      },
+      ConditionExpression: "attribute_not_exists(notesId)"  
+    }
+    await documentClient.put(param).promise();
+    callBack(null, send(201, event.body));
+  }catch(err) {
+    return send(500, JSON.stringify(err.message));
+  }
+};
+
+module.exports.updateNote = async (event) => {
+  const id = event.pathParameters.id;
+  return send(200, `Note ${id}, updated`);
+};
+
+module.exports.deleteNote = async (event) => {
+  const id = event.pathParameters.id;
+  return send(200, `Note ${id}, deleted`);
+};
+
+module.exports.getAllNotes = async (event) => {
+  return send(200, `All notes`);
+};
