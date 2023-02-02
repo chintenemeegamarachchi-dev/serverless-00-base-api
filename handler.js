da3c085ce7168c3e1579e51172854113e9f20b44
Chintana Meegamarachchi <chintana@appsolve.com.au>
2023-02-02 08:25:07 +0800

    Updates updateNote feature to update entry in DynamoDB

--- handler.js
+++ handler.js
@@ -30,9 +30,30 @@ module.exports.createNote = async (event, context, callBack) => {
   }
 };
 
-module.exports.updateNote = async (event) => {
-  const id = event.pathParameters.id;
-  return send(200, `Note ${id}, updated`);
+module.exports.updateNote = async (event, context, callBack) => {
+  const {id, title, ...body} = JSON.parse(event.body);
+  try {
+    const param = {
+      TableName: NOTES_TBL_NAME,
+      Key: {
+        notesId: id
+      },
+      UpdateExpression: "set #title = :title, #body = :body",
+      ExpressionAttributeNames: {
+        "#title": "title",
+        "#body": "body"
+      },
+      ExpressionAttributeValues: {
+        ":title": title,
+        ":body": body
+      },
+      ConditionExpression: "attribute_exists(notesId)"  
+    }
+    await documentClient.update(param).promise();
+    callBack(null, send(201, event.body));
+  }catch(err) {
+    return send(500, JSON.stringify(err.message));
+  }
 };
 
 module.exports.deleteNote = async (event) => {
